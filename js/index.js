268cace76a5b7433c299d093ce1bafdecfeda39c
t-williamss <33206136+t-williamss@users.noreply.github.com>
2021-05-06 12:14:30 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+setTimeout(function(){
+  document.querySelector('#boot').style.display = 'none';
+}, 3000);
+
+setTimeout(function(){
+  document.querySelector('#boot-ready').style.display = 'none';
+}, 0);
