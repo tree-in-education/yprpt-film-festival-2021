806a0d506a2163d7b74ce19b63c9e8f33379a639
t-williamss <33206136+t-williamss@users.noreply.github.com>
2021-06-18 15:41:43 -0700

    updates to content

--- js/index.js
+++
@@ -1,7 +0,0 @@
-setTimeout(function(){
-  document.querySelector('#boot').style.display = 'none';
-}, 3000);
-
-setTimeout(function(){
-  document.querySelector('#boot-ready').style.display = 'none';
-}, 0);
